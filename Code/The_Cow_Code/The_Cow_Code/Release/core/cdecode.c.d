917a6e6e95a4ee82794b217ccadd158d93d931af
Fadi Bunni <fadibunni_guitar@hotmail.com>
2019-04-05 19:27:29 +0200

    Added code

---
+++ Code/The_Cow_Code/The_Cow_Code/Release/core/cdecode.c.d
@@ -0,0 +1,7 @@
+C:\Users\fadib\AppData\Local\Temp\VMBuilds\The_Cow_Code\esp32_esp32\Release\core\cdecode.c.o: \
+ C:\Users\fadib\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.1\cores\esp32\libb64\cdecode.c \
+ C:\Users\fadib\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.1\cores\esp32\libb64\cdecode.h \
+ C:\Users\fadib\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.1/tools/sdk/include/newlib/stdint.h \
+ C:\Users\fadib\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.1/tools/sdk/include/newlib/machine/_default_types.h \
+ C:\Users\fadib\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.1/tools/sdk/include/newlib/sys/features.h \
+ C:\Users\fadib\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.1/tools/sdk/include/newlib/sys/_intsup.h
---
+++ Code/The_Cow_Code/The_Cow_Code/Release/core/cencode.c.d
@@ -0,0 +1,3 @@
+C:\Users\fadib\AppData\Local\Temp\VMBuilds\The_Cow_Code\esp32_esp32\Release\core\cencode.c.o: \
+ C:\Users\fadib\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.1\cores\esp32\libb64\cencode.c \
+ C:\Users\fadib\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.1\cores\esp32\libb64\cencode.h
